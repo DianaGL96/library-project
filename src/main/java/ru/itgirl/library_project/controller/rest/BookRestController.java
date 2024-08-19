4dd9cade3c4a7f5441748b0a6e28ed15b322b078
DianaGL96 <gorina.diana1996@gmail.com>
2024-08-19 12:39:04 +0300

    Add controller

--- src/main/java/ru/itgirl/library_project/controller/rest/BookRestController.java
+++ src/main/java/ru/itgirl/library_project/controller/rest/BookRestController.java
@@ -5,36 +5,36 @@ import org.springframework.web.bind.annotation.*;
 import ru.itgirl.library_project.dto.*;
 import ru.itgirl.library_project.service.BookService;
 
-@RequiredArgsConstructor
-@RestController
-public class BookRestController {
-    private final BookService bookService;
+//@RequiredArgsConstructor
+        //@RestController
+        //public class BookRestController {
+    //private final BookService bookService;
 
-    @GetMapping("/book")
-    BookDto getBookByName(@RequestParam("name") String name) {
-        return bookService.getByNameV1(name);
-    }
+    //@GetMapping("/book")
+    //BookDto getBookByName(@RequestParam("name") String name) {
+        //return bookService.getByNameV1(name);
+    //}
 
-    @GetMapping("/book/v2")
-    BookDto getBookByNameV2(@RequestParam("name") String name) {
-        return bookService.getByNameV2(name);
-    }
+    //@GetMapping("/book/v2")
+    //BookDto getBookByNameV2(@RequestParam("name") String name) {
+        //return bookService.getByNameV2(name);
+    //}
 
-    @GetMapping("/book/v3")
-    BookDto getBookByNameV3(@RequestParam("name") String name) {
-        return bookService.getByNameV3(name);
-    }
+    //@GetMapping("/book/v3")
+    //BookDto getBookByNameV3(@RequestParam("name") String name) {
+        //return bookService.getByNameV3(name);
+    //}
 
-    @PostMapping("/book/create")
-    BookDto createBook(@RequestBody BookCreateDto bookCreateDto) {
-        return bookService.createBook(bookCreateDto);
-    }
-    @PutMapping("/book/update")
-    BookDto updateBook(@RequestBody BookUpdateDto bookUpdateDto) {
-        return bookService.updateBook(bookUpdateDto);
-    }
-    @DeleteMapping("/book/delete/{id}")
-    void deleteBook(@PathVariable("id") Long id) {
-        bookService.deleteBook(id);
-    }
-}
+    //@PostMapping("/book/create")
+    //BookDto createBook(@RequestBody BookCreateDto bookCreateDto) {
+        //return bookService.createBook(bookCreateDto);
+    //}
+    //@PutMapping("/book/update")
+    //BookDto updateBook(@RequestBody BookUpdateDto bookUpdateDto) {
+        //return bookService.updateBook(bookUpdateDto);
+    //}
+    //@DeleteMapping("/book/delete/{id}")
+    //void deleteBook(@PathVariable("id") Long id) {
+        //bookService.deleteBook(id);
+    //}
+//}
